682f24ba55889387a7aa04343e38ac5e13854560
kittenyyj <47599595+kittenyyj@users.noreply.github.com>
2019-03-04 21:53:07 +0800

    Update paixu_mp.py

--- paixu_mp.py
+++ paixu_mp.py
@@ -3,7 +3,5 @@ b=len(a)
 for i in range(1,b):
     for j in range(1,b-i+1):
         if a[j-1]>a[j]:
-            t=a[j-1]
-            a[j-1]=a[j]
-            a[j]=t
+            a[j],a[j-1]=a[j-1],a[j]
 print(a)
